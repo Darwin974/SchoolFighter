2890b3092bb1ffc688f230d1c76c4b1bdb11cdd6
Darwin974 <maxime.rougemont974@gmail.com>
2025-03-17 09:20:16 +0400

    Update

--- main.py
+++ main.py
@@ -31,17 +31,35 @@ class ImageLoader:
 image_loader = ImageLoader()
 
 class AnimationLoop:
-    def __init__(self, image_loader, dossier, taille, x, y):
+    def __init__(self, image_loader, dossier, taille, x, y, appliquer_gravite=False):
         self.images = image_loader.charger_images(dossier, taille)
         self.sprite_actuel = 0
         self.vitesse_sprite = 0.2
         self.x = x
         self.y = y
+        self.vitesse_x = 0
+        self.vitesse_y = 0
+        self.en_air = False  # Indique si le joueur est en l'air
+        self.sol = 100  # Position du sol ajustée (plus élevée)
+
+        self.appliquer_gravite = appliquer_gravite
 
     def mettre_a_jour(self):
         self.sprite_actuel += self.vitesse_sprite
         if self.sprite_actuel >= len(self.images):
             self.sprite_actuel = 0  # Boucle l'animation
+        self.x += self.vitesse_x
+        self.y += self.vitesse_y
+
+        # Appliquer la gravité uniquement si nécessaire
+        if self.appliquer_gravite:
+            if self.y < self.sol:
+                self.vitesse_y += 0.5  # Gravité
+                self.en_air = True
+            else:
+                self.y = self.sol
+                self.vitesse_y = 0
+                self.en_air = False
 
     def dessiner(self, surface):
         surface.blit(self.images[int(self.sprite_actuel)], (self.x, self.y))
@@ -130,17 +148,62 @@ class EcranMenu:
         for button in self.buttons:
             button.showButton(surface)
 
+class CombatSystem:
+    def __init__(self, joueur_animation):
+        self.joueur_animation = joueur_animation
+        self.joueur_vie = 100
+        self.adversaire_vie = 100
+        self.combat_termine = False
+
+    def attaquer(self):
+        # Logique d'attaque
+        self.adversaire_vie -= 10
+        if self.adversaire_vie <= 0:
+            self.combat_termine = True
+
+    def defendre(self):
+        # Logique de défense
+        self.joueur_vie -= 5
+        if self.joueur_vie <= 0:
+            self.combat_termine = True
+
+    def mettre_a_jour(self):
+        # Mettre à jour l'état du combat
+        pass
+
+    def dessiner(self, surface):
+        # Dessiner les éléments du combat
+        font = pygame.font.Font(None, 36)
+        joueur_vie_text = font.render(f'Joueur Vie: {self.joueur_vie}', True, (255, 255, 255))
+        adversaire_vie_text = font.render(f'Adversaire Vie: {self.adversaire_vie}', True, (255, 255, 255))
+        surface.blit(joueur_vie_text, (10, 10))
+        surface.blit(adversaire_vie_text, (10, 50))
+
 class EcranJeu:
     def __init__(self):
         self.fond_animation = AnimationLoop(image_loader, 'img/IPI_fond_chill', (700, 700), 0, 0)
         self.debut_combat_animation = AnimationSprite(image_loader, 'img/IPI_fight', (700, 700))
-        self.joueur_animation = AnimationLoop(image_loader, 'img/IPI_Basic', (500, 500), -10, 125)
+        self.joueur_animation = AnimationLoop(image_loader, 'img/IPI_Basic', (500, 500), 100, 200, appliquer_gravite=True)  # Position initiale ajustée
         self.debut_combat_termine = False
+        self.combat_system = CombatSystem(self.joueur_animation)
 
     def gerer_evenements(self, evenement):
         if evenement.type == pygame.KEYDOWN:
             if evenement.key == pygame.K_ESCAPE:
                 return 'menu'  # Retourner au menu
+            elif evenement.key == pygame.K_a:
+                self.combat_system.attaquer()
+            elif evenement.key == pygame.K_d:
+                self.combat_system.defendre()
+            elif evenement.key == pygame.K_LEFT:
+                self.joueur_animation.vitesse_x = -5
+            elif evenement.key == pygame.K_RIGHT:
+                self.joueur_animation.vitesse_x = 5
+            elif evenement.key == pygame.K_UP and not self.joueur_animation.en_air:
+                self.joueur_animation.vitesse_y = -10  # Sauter
+        elif evenement.type == pygame.KEYUP:
+            if evenement.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+                self.joueur_animation.vitesse_x = 0
         return 'jeu'
 
     def mettre_a_jour(self):
@@ -151,6 +214,7 @@ class EcranJeu:
                 self.debut_combat_termine = True
         else:
             self.joueur_animation.mettre_a_jour()
+            self.combat_system.mettre_a_jour()
 
     def dessiner(self, surface):
         surface.fill((0, 0, 0))
@@ -159,6 +223,7 @@ class EcranJeu:
             self.debut_combat_animation.dessiner(surface, 0, 0)
         else:
             self.joueur_animation.dessiner(surface)
+            self.combat_system.dessiner(surface)
 
 # Initialiser les écrans
 ecrans = {
